f3d16e50bda13ea05266d3bc8d18a433f907c635
Lakshy <lakshkuldeep16@gmail.com>
2026-08-15 00:37:54 +0530

    Bump PricePilot version to 1.1 for clean APK updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
     applicationId = "com.aistudio.pricepilot.pqrst"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     buildConfigField("String", "PRICE_API_BASE_URL", "\"$priceApiBaseUrl\"")
   }
